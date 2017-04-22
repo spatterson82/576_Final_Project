e304c5af4d37f076c8849dbc1e3c968b497ce755
Stephen <spatterson8@wisc.edu>
2017-04-22 19:52:28 -0400

    576 Final - Added csv file of houses and added points on map.

--- js/main.js
+++ js/main.js
@@ -1,57 +1,22 @@
-var mymap = L.map('mapid').setView([-104.99404, 39.75621], 13);
+(function() {
+    'use strict';
 
-// mapbox tiles
-// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox.satellite',
-//     accessToken: 'pk.eyJ1Ijoic3BhdHRlcnNvbjgiLCJhIjoiY2lzZzBnbmlxMDFzNjJzbnZ1cXJ0bDJ5cSJ9.r_0eIQ9LIuNS3LV-GL1AIg'
-// }).addTo(mymap);
+    function createMap() {
+        var map = L.map('mapid', {
+            center: [43, -71],
+            zoom: 11
+        });
+        //add OSM base tilelayer
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-//openstreetmap tiles
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-}).addTo(mymap);
-
-// // Markers
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
-
-var layer = L.marker(latlng, {
-    title: feature.properties.gage
-});
-
-var circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(mymap);
-
-var polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(mymap);
-
-//Pop-up Layer
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup(); //only on marker objects
-circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon");
-
-var popup = L.popup()
-    .setLatLng([51.5, -0.09])
-    .setContent("I am a standalone popup.")
-    .openOn(mymap);
-
-//Events
-var popup = L.popup();
-
-function onMapClick(e) {
-    popup   
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);
-}
+        $.get('data/listings.csv', function (csvContents) {
+            var geoLayer = L.geoCsv(csvContents, {firstLineTitles: true, fieldSeparator: ','});
+            console.log(geoLayer);
+            map.addLayer(geoLayer);
+        });
+    }
 
 
-mymap.on('click', onMapClick);
+    $(document).ready(createMap);
+
+})();

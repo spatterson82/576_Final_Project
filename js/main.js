41c779ec1aa10048494c15437b7f9e4d08a7065c
brbadger <mvetter2@wisc.edu>
2017-05-01 20:48:04 -0500

    576 final- replace with

--- js/main.js
+++ js/main.js
@@ -32,10 +32,10 @@
                         layer.closePopup();
                     });
                     layer.on("click", function (e){
-                        $("#AddressResult").text(csvContents.properties['address'])
-                        $("#PriceResult").text(csvContents.properties['price2'])
-                        $("#CityResult").text(csvContents.properties['city'])
-                        $("#ZipResult").text(csvContents.properties['zip'])
+                        $("#AddressResult p").replaceWith(csvContents.properties['address'])
+                        $("#PriceResult p").text(csvContents.properties['price2'])
+                        $("#CityResult p").text(csvContents.properties['city'])
+                        $("#ZipResult p").text(csvContents.properties['zip'])
                         $("#Picture").attr({"src": csvContents.properties['photo']})
                     });
                 },
@@ -176,10 +176,10 @@
                         layer.closePopup();
                     });
                     layer.on("click", function (e){
-                        $("#AddressResult").text(csvContents.properties['address'])
-                        $("#PriceResult").text(csvContents.properties['price2'])
-                        $("#CityResult").text(csvContents.properties['city'])
-                        $("#ZipResult").text(csvContents.properties['zip'])
+                        $("#AddressResult p").text(csvContents.properties['address'])
+                        $("#PriceResult p").text(csvContents.properties['price2'])
+                        $("#CityResult p").text(csvContents.properties['city'])
+                        $("#ZipResult p").text(csvContents.properties['zip'])
                         $("#Picture").attr({"src": csvContents.properties['photo']})
                     });
                 },
